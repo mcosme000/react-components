d3b3c3b702178c2ef23ff22a3e1ab1021bb3c73f
Maria Cosmen <mcosme00@gmail.com>
2023-04-19 15:47:56 +0900

    Move function outside map method

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -7,13 +7,17 @@ const Accordion = ({ items }) => {
   // we set the first section as expanded
   const [expanded, setExpanded] = useState(0)
 
+  const handleClick= (index) => {
+    setExpanded(index)
+  }
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expanded
 
     return (
       <div key={item.id} className={isExpanded ? 'expanded' : 'collapsed'}>
         <div className="flex items-center">
-          <h3 onClick={() => setExpanded(index)}>{item['label']}</h3>
+          <h3 onClick={() => handleClick(index)}>{item['label']}</h3>
           {isExpanded ? <BiCaretUp /> : <BiCaretDown />}
         </div>
         {isExpanded && <div>{item.content}</div>}
