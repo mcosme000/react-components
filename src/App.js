60ae008eea600204773d10ac96db859d6a9b7a51
Maria Cosmen <mcosme00@gmail.com>
2023-04-19 12:48:56 +0900

    Add logic to display expanded or collapsed content

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ const App = () => {
 
   const items = [
     {
+      id: 1,
       label: 'Lemon Tarts',
       content: 'THe lemon is amazin in tarts and smells good'
     },
     {
+      id: 2,
       label: 'About Tailwind',
       content: 'Best practices and things to avoid while using Tailwind'
     },
     {
+      id: 3,
       label: 'Can I use CSS on a project?',
       content: 'Should I go to CSS or use SASS?'
     }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,24 +1,34 @@
 import { BiCaretDown } from "react-icons/bi";
 import { BiCaretUp } from "react-icons/bi";
+import { useState } from 'react'
+import '../styles/accordion.css'
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  // we set the first section as expanded
+  const [expanded, setExpanded] = useState(0)
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expanded
+
     return (
-      <div>
+      <div key={item.id} className={isExpanded ? 'expanded' : 'collapsed'}>
         <div className="flex items-center">
-          <h3>{item['label']}</h3>
-          <BiCaretDown />
+          <h3 onClick={() => setExpanded(index)}>{item['label']}</h3>
+          {isExpanded ? <BiCaretUp /> : <BiCaretDown />}
         </div>
-        <p>{item['content']}</p>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     )
   })
 
   return(
-    <div>
+    <div className="w-auto bg-blue-300">
       {renderedItems}
     </div>
   )
 }
 
 export default Accordion
+
+// About the const content:
+// when comparing with &&, it will return the last truthy value
