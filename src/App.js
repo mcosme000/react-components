bb5209fb00341d6c8e15371d59c246bb11035980
Maria Cosmen <mcosme00@gmail.com>
2023-04-20 10:30:23 +0900

    Create Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import ButtonPage from './pages/ButtonPage'
-import AccordionPage from './pages/AccordionPage'
 import Dropdown from './components/Dropdown'
 
 const App = () => {
 
+  const options = [
+    {
+      id: 1,
+      label: 'I don\'t like spicy food',
+      value: 'no_spicy'
+    },
+    {
+      id: 2,
+      label: 'Mild curry',
+      value: 'mild'
+    },
+    {
+      id: 3,
+      label: 'I can eat spicy foods',
+      value: 'spicy'
+    },
+    {
+      id: 4,
+      label: 'I like seeing the world burn',
+      value: 'extra spicy'
+    }
+  ]
+
+
   return (
     <div className="components">
-      <AccordionPage />
-      <ButtonPage />
-      <Dropdown />
+      <Dropdown options={options}/>
     </div>
   )
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,35 @@
-const Dropdown = () => {
+import { useState } from 'react'
+
+const Dropdown = ({options}) => {
+  const [show, setShow] = useState(false)
+  const [option, setOption] = useState('')
+
+
+  const showDropdown = () => {
+    setShow(!show)
+  }
+
+  const handleOption = (option) => {
+    setOption(option.label)
+    setShow(false)
+  }
+
+  const select = option || 'Select'
+  const renderOptions = options.map((option, index) => {
+    return (
+      <div key={index}>
+        <p onClick={() => handleOption(option)}>{option.label}</p>
+      </div>
+    )
+  })
+
   return (
-    <div>
-      <h1>Dropdown</h1>
+    <div className="p-5">
+      <h1>Choose the spicy level 🔥</h1>
+      <h3 onClick={showDropdown}>{select}</h3>
+      <div>
+        {show && renderOptions }
+      </div>
     </div>
   )
 }
