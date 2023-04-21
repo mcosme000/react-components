ae8768d51324da79e7832a67e3b498c09999c085
Maria Cosmen <mcosme00@gmail.com>
2023-04-21 22:55:29 +0900

    Hover state styles

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
+
 
 const Dropdown = ({options, selection, onSelect}) => {
   const [show, setShow] = useState(false)
@@ -17,7 +19,7 @@ const Dropdown = ({options, selection, onSelect}) => {
   const renderOptions = options.map((option, index) => {
     return (
       <div key={index}>
-        <p onClick={() => handleOption(option)} className="px-3 py-2 border-b">{option.label}</p>
+        <p onClick={() => handleOption(option)} className="px-3 py-2 border-b hover:bg-blue-200">{option.label}</p>
       </div>
     )
   })
@@ -26,7 +28,10 @@ const Dropdown = ({options, selection, onSelect}) => {
     <div className="p-5 w-80">
       <h1 className="mb-1.5 p-1">Choose the spicy level 🔥</h1>
       <div>
-        <h3 onClick={showDropdown} className="px-3 py-2 border-2 rounded cursor-pointer">{select}</h3>
+        <div onClick={showDropdown} className="flex items-center justify-between px-3 py-2 border-2 border-blue-500 rounded cursor-pointer">
+          <h3>{select}</h3>
+          {show ? <BiChevronUp /> : <BiChevronDown/>}
+        </div>
         {show && <div className="mt-1.5 border-2 rounded cursor-pointer">{renderOptions}</div>}
       </div>
     </div>
