05707d8d75ccaddab5adb4853717aa044628c10a
Maria Cosmen <mcosme00@gmail.com>
2023-04-23 17:20:59 +0900

    useEffect and useRef implementation

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 import Panel from './Panel'
 
 const Dropdown = ({options, selection, onSelect}) => {
   const [show, setShow] = useState(false)
+
+  // our reference is our dropdown component
+  const divEl = useRef();
+
+  // this gets called when we first load the screen
+  useEffect(() => {
+    const handler = (event) => {
+      // if the click is not inside the dropdown, we want to hide it.
+      if (!divEl.current.contains(event.target)) {
+        setShow(false)
+      };
+    };
+
+    document.addEventListener('click', handler, true);
+
+    // this gets called when our dropdown is removed from the screen
+    return () => {
+      document.removeEventListener('click', handler)
+    };
+  }, []);
+
 
   const showDropdown = () => {
     setShow(!show)
@@ -24,9 +45,8 @@ const Dropdown = ({options, selection, onSelect}) => {
     )
   })
 
-
   return (
-    <div className="p-5 w-80">
+    <div ref={divEl} className="p-5 w-80">
       <h1 className="mb-1.5 p-1">Choose the spicy level 🔥</h1>
       <div>
         <Panel onClick={showDropdown} className="flex items-center justify-between border-blue-500">
